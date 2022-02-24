84f9889a4a75d158e9485c1f02681d65282aa557
Nitin Mishra <mnitin2311@gmail.com>
2022-02-25 00:53:51 +0530

    working on features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { fabric } from "fabric";
 import styled from "styled-components";
 
 function App() {
-  useEffect(() => {});
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    // setBackground(
+    //   "https://cdn.pixabay.com/photo/2017/03/17/19/37/sky-2152463_960_720.jpg",
+    //   canvas
+    // );
+
+    canvas.on("mouse:wheel", function (opt) {
+      var delta = opt.e.deltaY;
+      var zoom = canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 0.01) zoom = 0.01;
+      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+      var vpt = this.viewportTransform;
+      if (zoom < 400 / 1000) {
+        vpt[4] = 200 - (1000 * zoom) / 2;
+        vpt[5] = 200 - (1000 * zoom) / 2;
+      } else {
+        if (vpt[4] >= 0) {
+          vpt[4] = 0;
+        } else if (vpt[4] < canvas.getWidth() - 1000 * zoom) {
+          vpt[4] = canvas.getWidth() - 1000 * zoom;
+        }
+        if (vpt[5] >= 0) {
+          vpt[5] = 0;
+        } else if (vpt[5] < canvas.getHeight() - 1000 * zoom) {
+          vpt[5] = canvas.getHeight() - 1000 * zoom;
+        }
+      }
+    });
+  }, []);
 
   const initCanvas = (id) => {
     return new fabric.Canvas(id, {
@@ -13,6 +47,8 @@ function App() {
   };
 
   const setBackground = (url, canvas) => {
+    console.log(URL.createObjectURL(url));
+
     fabric.Image.fromURL(url, (img) => {
       canvas.backgroundImage = img;
       canvas.renderAll();
@@ -21,41 +57,61 @@ function App() {
 
   const canvas = initCanvas("canvas");
 
-  setBackground(
-    "https://cdn.pixabay.com/photo/2017/03/17/19/37/sky-2152463_960_720.jpg",
-    canvas
-  );
-  canvas.on("mouse:wheel", function (opt) {
-    var delta = opt.e.deltaY;
-    var zoom = canvas.getZoom();
-    zoom *= 0.999 ** delta;
-    if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
-    canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-    opt.e.preventDefault();
-    opt.e.stopPropagation();
-    var vpt = this.viewportTransform;
-    if (zoom < 400 / 1000) {
-      vpt[4] = 200 - (1000 * zoom) / 2;
-      vpt[5] = 200 - (1000 * zoom) / 2;
-    } else {
-      if (vpt[4] >= 0) {
-        vpt[4] = 0;
-      } else if (vpt[4] < canvas.getWidth() - 1000 * zoom) {
-        vpt[4] = canvas.getWidth() - 1000 * zoom;
-      }
-      if (vpt[5] >= 0) {
-        vpt[5] = 0;
-      } else if (vpt[5] < canvas.getHeight() - 1000 * zoom) {
-        vpt[5] = canvas.getHeight() - 1000 * zoom;
-      }
+  const imageChange = (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0]);
     }
-  });
+  };
+  console.log(selectedImage);
+
+  // This function will be triggered when the "Remove This Image" button is clicked
+  const removeSelectedImage = () => {
+    setSelectedImage();
+  };
 
   return (
     <Container>
       <Canvas>
         <canvas id="canvas" />
+        <Image>
+          <Button
+            type="file"
+            accept="image/*"
+            onChange={imageChange}
+            // onChange={(event) => {
+            //   setSelectedImage(event.target.files[0]);
+            // }}
+          />
+          <button
+            style={{
+              background: "green",
+              border: "none",
+              padding: "10px",
+              margin: "10px 0",
+              width: "100px",
+              borderRadius: "5px",
+              color: "white",
+            }}
+            onClick={() => {
+              setBackground(selectedImage, canvas);
+            }}
+          >
+            Upload!
+          </button>
+          <button
+            onClick={removeSelectedImage}
+            style={{
+              background: "red",
+              border: "none",
+              padding: "10px",
+              width: "100px",
+              borderRadius: "5px",
+              color: "white",
+            }}
+          >
+            Remove{" "}
+          </button>
+        </Image>
       </Canvas>
     </Container>
   );
@@ -78,4 +134,13 @@ const Canvas = styled.div`
     height: 400px;
     cursor: all-scroll;
   }
+`;
+const Image = styled.div`
+  padding: 10px;
+  width: 30px;
+  height: 30px;
+`;
+const Button = styled.input`
+  background-color: burlywood;
+  border-radius: 10px;
 `;
